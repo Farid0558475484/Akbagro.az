04f1e3dfc6a89489acbc68b34d8da5ccc21a2fca
Farid0558475484 <faridmh@code.edu.az>
2023-04-10 03:50:38 +0400

    feat:add producat name and pagination

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Pagination from "react-bootstrap/Pagination";
 import s from "./Product.module.scss";
 import { cards } from "./../../data/cards.js";
 
 function Products() {
+  const [activePage, setActivePage] = useState(1);
+  const cardsPerPage = 8;
+  const totalPages = Math.ceil(cards.length / cardsPerPage);
+
+  const handleClick = (page) => {
+    setActivePage(page);
+  };
+
+  const cardsToDisplay = cards.slice(
+    (activePage - 1) * cardsPerPage,
+    activePage * cardsPerPage
+  );
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.min(5, totalPages); i++) {
+    pageNumbers.push(i);
+  }
+
+  const firstPage = Math.max(1, activePage - 2);
+  const lastPage = Math.min(totalPages, activePage + 2);
+
+  for (let i = firstPage; i <= lastPage; i++) {
+    if (!pageNumbers.includes(i)) {
+      if (i === 1) {
+        pageNumbers.unshift(i);
+      } else if (i === totalPages) {
+        pageNumbers.push(i);
+      } else if (pageNumbers.length < 5) {
+        pageNumbers.push(i);
+      } else {
+        pageNumbers.splice(2, 1, "...");
+        break;
+      }
+    }
+  }
+
   return (
     <Container>
       <Row className="mt-5 mb-5">
-        {cards.map((card, index) => (
+        {cardsToDisplay.map((card, index) => (
           <Col xl="3" xs="6" key={index} className={s.cardCol}>
             <Card className={s.card}>
               <img
@@ -29,6 +66,27 @@ function Products() {
           </Col>
         ))}
       </Row>
+      {totalPages > 1 && (
+        <Pagination className="justify-content-center">
+          <Pagination.Prev
+            disabled={activePage === 1}
+            onClick={() => handleClick(activePage - 1)}
+          />
+          {pageNumbers.map((pageNumber) => (
+            <Pagination.Item
+              key={pageNumber}
+              active={pageNumber === activePage}
+              onClick={() => handleClick(pageNumber)}
+            >
+              {pageNumber}
+            </Pagination.Item>
+          ))}
+          <Pagination.Next
+            disabled={activePage === totalPages}
+            onClick={() => handleClick(activePage + 1)}
+          />
+        </Pagination>
+      )}
     </Container>
   );
 }
--- src/data/cards.js
+++ src/data/cards.js
@@ -1,487 +1,476 @@
 export const cards = [
   {
     imageSrc: "./imgProduct/1.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Yuvarlaq damlama borusu",
   },
   {
     imageSrc: "./imgProduct/2.webp",
-    title: "Kurtağzı çıxış nipeli",
-  },
-  {
-    imageSrc: "./imgProduct/3.webp",
-    title: "Kurtağzı çıxış nipeli",
-  },
-  {
-    imageSrc: "./imgProduct/4.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "PC basınca ayarlı boru",
   },
   {
     imageSrc: "./imgProduct/5.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Priz kolye",
   },
   {
     imageSrc: "./imgProduct/6.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Priz kolye galvanized bold",
   },
   {
     imageSrc: "./imgProduct/7.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin keçid tee",
   },
+
   {
     imageSrc: "./imgProduct/8.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin dirsək çöl diş",
   },
   {
     imageSrc: "./imgProduct/9.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin qapaq",
   },
   {
     imageSrc: "./imgProduct/10.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Vantuz",
   },
   {
     imageSrc: "./imgProduct/11.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Süzgəc klapan",
   },
   {
     imageSrc: "./imgProduct/12.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sabitləmə çubuğu",
   },
   {
     imageSrc: "./imgProduct/13.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Monometr quru",
   },
   {
     imageSrc: "./imgProduct/14.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Monometr qlisirinli",
   },
   {
     imageSrc: "./imgProduct/15.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin açar",
   },
   {
     imageSrc: "./imgProduct/16.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Dəlici matkap",
   },
   {
     imageSrc: "./imgProduct/17.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Dəlici 3 mm",
   },
   {
     imageSrc: "./imgProduct/18.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Can su",
   },
   {
     imageSrc: "./imgProduct/19.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Boru asqısı",
   },
   {
     imageSrc: "./imgProduct/20.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Bağlama ipi",
   },
   {
     imageSrc: "./imgProduct/21.webp",
-    title: "Kurtağzı çıxış nipeli",
-  },
-  {
-    imageSrc: "./imgProduct/21.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Gübrə qarışdırıcı xrom",
   },
   {
     imageSrc: "./imgProduct/22.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Gübrə qarışdırıcı plastik",
   },
   {
     imageSrc: "./imgProduct/23.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Bypass tip hidrosiklon plastik filtrli gravel sistem",
   },
   {
     imageSrc: "./imgProduct/24.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Yarıavtomatik seperatorlu double filtrli sistem",
   },
   {
     imageSrc: "./imgProduct/25.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Yarıavtomatik plastik filtrli gravel sistem",
   },
   {
     imageSrc: "./imgProduct/26.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Yarıavtomatik double filtrli sistem",
   },
   {
     imageSrc: "./imgProduct/27.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Seperator",
   },
   {
     imageSrc: "./imgProduct/28.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Hidrosiklon",
   },
   {
     imageSrc: "./imgProduct/29.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Gübrə çənləri",
   },
   {
     imageSrc: "./imgProduct/30.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Gravel filtr",
   },
   {
     imageSrc: "./imgProduct/31.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Metal ayaqlı filtr",
   },
   {
     imageSrc: "./imgProduct/32.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Plastik double filtr",
   },
   {
     imageSrc: "./imgProduct/33.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Plastik mini filtr",
   },
   {
     imageSrc: "./imgProduct/34.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Plastik filtr",
   },
   {
     imageSrc: "./imgProduct/35.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kələbək vana plastik",
   },
   {
     imageSrc: "./imgProduct/36.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kələbək vana metal",
   },
   {
     imageSrc: "./imgProduct/37.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Gübrələmə vanası",
   },
   {
     imageSrc: "./imgProduct/38.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana iç diş yaşıl Poelsan",
   },
   {
     imageSrc: "./imgProduct/39.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana çöl diş yaşıl Poelsan",
   },
   {
     imageSrc: "./imgProduct/40.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana iç diş mavi Poelsan",
   },
   {
     imageSrc: "./imgProduct/41.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana mavi",
   },
   {
     imageSrc: "./imgProduct/42.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana yaşıl Poelsani",
   },
   {
     imageSrc: "./imgProduct/43.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana mavi",
   },
   {
     imageSrc: "./imgProduct/44.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana yaşıl Poelsan",
   },
   {
     imageSrc: "./imgProduct/45.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kaplin vana mavi Poelsan",
   },
   {
     imageSrc: "./imgProduct/46.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kürəsəl vana iç-çöl diş Poelsan",
   },
   {
     imageSrc: "./imgProduct/47.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kürəsəl vana iç diş mavi Poelsan",
   },
   {
     imageSrc: "./imgProduct/48.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "İç dişli nipel",
   },
   {
     imageSrc: "./imgProduct/49.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "İç dişli dirsək",
   },
   {
     imageSrc: "./imgProduct/50.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Çöl-iç dişli nipel",
   },
   {
     imageSrc: "./imgProduct/51.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Çöl dişli tapa",
   },
   {
     imageSrc: "./imgProduct/52.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Çöl dişli red nipel",
   },
   {
     imageSrc: "./imgProduct/53.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Çöl dişli nipel",
   },
   {
     imageSrc: "./imgProduct/54.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Çol dişli dirsək",
   },
   {
     imageSrc: "./imgProduct/55.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Çöl dişli tee",
   },
   {
     imageSrc: "./imgProduct/56.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler Berksan",
   },
   {
     imageSrc: "./imgProduct/57.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler sadə",
   },
   {
     imageSrc: "./imgProduct/58.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler yarasa tipli",
   },
   {
     imageSrc: "./imgProduct/59.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler 1453 plastik",
   },
   {
     imageSrc: "./imgProduct/60.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler 70 M - JET",
   },
   {
     imageSrc: "./imgProduct/61.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler tənzimlənən",
   },
   {
     imageSrc: "./imgProduct/62.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mikro turbo sprink başlığı",
   },
   {
     imageSrc: "./imgProduct/63.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mikro sisləmə dəsti",
   },
   {
     imageSrc: "./imgProduct/64.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mikro sprinkler başlıqları",
   },
   {
     imageSrc: "./imgProduct/65.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinkler nipeli",
   },
   {
     imageSrc: "./imgProduct/66.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Sprinker vanası",
   },
   {
     imageSrc: "./imgProduct/67.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Abot",
   },
   {
     imageSrc: "./imgProduct/68.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı uzatma borusu",
   },
   {
     imageSrc: "./imgProduct/69.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı tee",
   },
   {
     imageSrc: "./imgProduct/70.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı qapaq erkək",
   },
   {
     imageSrc: "./imgProduct/71.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı qapaq dişi",
   },
   {
     imageSrc: "./imgProduct/72.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı istavroz",
   },
   {
     imageSrc: "./imgProduct/73.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı xətt vana",
   },
   {
     imageSrc: "./imgProduct/74.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı dirsək",
   },
   {
     imageSrc: "./imgProduct/75.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı conta",
   },
   {
     imageSrc: "./imgProduct/76.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı dişi başlıq",
   },
   {
     imageSrc: "./imgProduct/77.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mandallı erkək başlıq",
   },
   {
     imageSrc: "./imgProduct/78.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Yağmurlama borusu",
   },
   {
     imageSrc: "./imgProduct/79.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Basınc ayarlı 16LT",
   },
   {
     imageSrc: "./imgProduct/80.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Basınc ayarlı 12LT",
   },
   {
     imageSrc: "./imgProduct/81.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Basınc ayarlı 8LT",
   },
   {
     imageSrc: "./imgProduct/82.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Basınc ayarlı 4LT",
   },
   {
     imageSrc: "./imgProduct/83.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Basınc ayarlı 2LT",
   },
   {
     imageSrc: "./imgProduct/84.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Tənzimlənən damladıcı (0 – 100 Lt/s)",
   },
   {
     imageSrc: "./imgProduct/85.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Tənzimlənən damladıcı (0 – 70 Lt/s)",
   },
   {
     imageSrc: "./imgProduct/86.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana üzüklü çöl dişli",
   },
   {
     imageSrc: "./imgProduct/87.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana somunlu conta çıxış",
   },
   {
     imageSrc: "./imgProduct/88.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana somunlu conta çıxış",
   },
   {
     imageSrc: "./imgProduct/89.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana somunlu",
   },
   {
     imageSrc: "./imgProduct/90.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana kilidli bağlantı",
   },
   {
     imageSrc: "./imgProduct/91.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana kurtağzı çöl dişli",
   },
   {
     imageSrc: "./imgProduct/92.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana kurtağzı conta çıxış",
   },
   {
     imageSrc: "./imgProduct/93.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Mini vana kurtağzı",
   },
   {
     imageSrc: "./imgProduct/94.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Üzüklü qapaq",
   },
   {
     imageSrc: "./imgProduct/95.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Üzüklü tee",
   },
   {
     imageSrc: "./imgProduct/99.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kilidli bağlantı qapaq",
   },
   {
     imageSrc: "./imgProduct/100.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kilidli bağlantı dirsək",
   },
   {
     imageSrc: "./imgProduct/101.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kilidli bağlantı tee",
   },
   {
     imageSrc: "./imgProduct/102.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kilidli bağlantı mufta",
   },
   {
     imageSrc: "./imgProduct/103.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kilidli bağlantı dişi adaptor",
   },
   {
     imageSrc: "./imgProduct/104.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kilidli bağlantı erkək adaptor",
   },
   {
     imageSrc: "./imgProduct/105.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu paylayıcı altılı",
   },
   {
     imageSrc: "./imgProduct/106.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu paylayıcı dördlü",
   },
   {
     imageSrc: "./imgProduct/107.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu paylayıcı ikili",
   },
   {
     imageSrc: "./imgProduct/108.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu çıxış nipeli",
   },
   {
     imageSrc: "./imgProduct/109.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu qapaq",
   },
   {
     imageSrc: "./imgProduct/110.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu dirsək",
   },
   {
     imageSrc: "./imgProduct/111.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu tee",
   },
   {
     imageSrc: "./imgProduct/112.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu erkək adaptor",
   },
   {
     imageSrc: "./imgProduct/113.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Somunlu ekleme nipel",
   },
   {
     imageSrc: "./imgProduct/114.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı paylayıcı altılı",
   },
   {
     imageSrc: "./imgProduct/115.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı paylayıcı dörtlü",
   },
   {
     imageSrc: "./imgProduct/116.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı paylayıcı ikili",
   },
   {
     imageSrc: "./imgProduct/117.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kuratğzı gözlük qapaq",
   },
   {
     imageSrc: "./imgProduct/118.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı tee",
   },
   {
     imageSrc: "./imgProduct/119.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı qapaq",
   },
   {
     imageSrc: "./imgProduct/120.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı erkək nipel",
   },
   {
     imageSrc: "./imgProduct/121.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı erkək dirsək",
   },
   {
     imageSrc: "./imgProduct/122.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı ekleme nipeli",
   },
   {
     imageSrc: "./imgProduct/123.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Kurtağzı dirsək",
   },
   {
     imageSrc: "./imgProduct/124.webp",
@@ -489,34 +478,34 @@ export const cards = [
   },
   {
     imageSrc: "./imgProduct/125.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "PE tee inegale",
   },
   {
     imageSrc: "./imgProduct/126.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "PE reduksion",
   },
   {
     imageSrc: "./imgProduct/127.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "PE qapaq",
   },
   {
     imageSrc: "./imgProduct/128.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "PE flans adaptor",
   },
   {
     imageSrc: "./imgProduct/129.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "PE dirsək",
   },
   {
     imageSrc: "./imgProduct/130.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Flans female",
   },
   {
     imageSrc: "./imgProduct/131.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Flans dəmir",
   },
   {
     imageSrc: "./imgProduct/132.webp",
-    title: "Kurtağzı çıxış nipeli",
+    title: "Polietilen borular",
   },
 ];
